5f27abbe3d3b27c59f8f9706031261f8578be09b
lucynyuu <nyuu@my.yorku.ca>
2023-05-02 15:11:26 -0400

    Setup the skeleton for the website

--- js/Ultraviolet.js
+++ js/Ultraviolet.js
@@ -1,5 +1,7 @@
 let about = document.getElementById("about");
-var aboutBox = document.getElementById("about-box");
+let aboutBox = document.getElementById("about-box");
+let newh = document.getElementById("newh");
+let centerBox = document.getElementById("center-box");
 
 about.onclick = function() {
     aboutBox.style.display = "block";
@@ -9,4 +11,24 @@ window.onclick = function(event) {
     if (event.target == aboutBox) {
         aboutBox.style.display = "none";
     }
+}
+
+newh.onclick = function() {
+    let address = document.createElement("div");
+    let dump = document.createElement("div");
+    $(address).attr("class", "values");
+    $(address).attr("id", "address");
+    $(address).html("Address");
+    $(dump).attr("class", "values");
+    $(dump).attr("id", "dump");
+    $(dump).html("Dump");
+    $(centerBox).append(address);
+    for(let i=0;i<16;i++) {
+        let nDiv = document.createElement("div");
+        $(nDiv).attr("class", "values");
+        $(nDiv).attr("id", i.toString(16));
+        $(nDiv).html(i.toString(16));
+        $(centerBox).append(nDiv);
+    }
+    $(centerBox).append(dump);
 }
